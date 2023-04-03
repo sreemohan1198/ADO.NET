d50f8103f27668a54930c54bfa4f1dbd83415a77
sridevi sribhashyam <sribhashyamsree11@gmail.com>
2023-04-03 14:37:47 -0400

    My ADO.NET EXAMPLES

---
+++ Ado.net_exmples/TeacherDTO.cs/TeachDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherDTO.cs
+{
+    public class TeachDTO
+    {
+        public int TEACHER_ID { get; set; }
+        public String TEACHER_NAME { get; set; }
+        public int AGE { get; set; }
+    }
+}
